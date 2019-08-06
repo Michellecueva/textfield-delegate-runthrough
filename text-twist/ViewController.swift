a9e0c4d500c414318b923a6b7f6b6e35f2ce9467
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-06 12:31:27 -0400

    Creates outlets

--- text-twist/ViewController.swift
+++ text-twist/ViewController.swift
@@ -11,7 +11,18 @@ import UIKit
 class ViewController: UIViewController, UITextFieldDelegate {
 
     
+    @IBOutlet weak var showAvailableLettersLabel: UILabel!
+    @IBOutlet weak var userGuessMessageLabel: UILabel!
+    
     @IBOutlet weak var inputTextField: UITextField!
+    
+    @IBOutlet weak var threeLettersTextView: UITextView!
+    
+    @IBOutlet weak var fourLettersTextView: UITextView!
+    
+    @IBOutlet weak var fiveLettersTextView: UITextView!
+    
+    @IBOutlet weak var sixLettersTextView: UITextView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
