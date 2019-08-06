77b9743f7a4747b06e08b970cf63597397121b89
davidlawrencer <davidlrifkin+github@gmail.com>
2019-08-06 12:00:21 -0400

    Adds label, textfield, and textviews to storyboard

--- text-twist/ViewController.swift
+++ text-twist/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var inputTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
